777536918ee4f622ca7de8081f0be14e3aec0587
Ahmet Altay <ahmetaltay33@users.noreply.github.com>
2019-02-01 19:35:19 +0300

    Create a empty project using create-react-app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>This is the first version of project, seems like a empty :)</header>
+      </div>
+    );
+  }
+}
+
+export default App;
