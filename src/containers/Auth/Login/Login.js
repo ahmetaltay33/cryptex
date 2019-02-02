c0090b7da5a2b71d1f41a6dc2900a9da00e36994
Ahmet Altay <ahmetaltay33@users.noreply.github.com>
2019-02-02 12:22:03 +0300

    npm install firebase and connected to the firebase email auth mechanizm

--- src/containers/Auth/Login/Login.js
+++ src/containers/Auth/Login/Login.js
@@ -4,6 +4,8 @@ import TextBox from 'devextreme-react/text-box';
 import Button from 'devextreme-react/button';
 import Box, { Item } from 'devextreme-react/box'
 import classes from './Login.module.css';
+import axios from 'axios';
+import firebase from "firebase";
 
 export class login extends Component {
     //static propTypes = {
@@ -11,16 +13,17 @@ export class login extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            userName: '',
+            eMail: '',
             password: ''
         };
-        this.userNameChangeHandle = this.userNameChangeHandle.bind(this);
+        this.eMailChangeHandle = this.eMailChangeHandle.bind(this);
         this.passwordChangeHandle = this.passwordChangeHandle.bind(this);
+        this.loginClickHandle = this.loginClickHandle.bind(this);
     }
 
-    userNameChangeHandle(e) {
+    eMailChangeHandle(e) {
         this.setState({
-            userName: e.value
+            eMail: e.value
         });
     }
 
@@ -28,6 +31,16 @@ export class login extends Component {
         this.setState({
             password: e.value
         });
+    }
+
+    loginClickHandle(e) {
+        firebase.auth().signInWithEmailAndPassword(this.state.eMail, this.state.password)
+            .then(response => {
+                console.log(response);        
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }
 
     render() {
@@ -40,7 +53,7 @@ export class login extends Component {
                     <Item ratio={1}>
                         <TextBox
                             //value={this.state.userName}
-                            onValueChanged={this.userNameChangeHandle}
+                            onValueChanged={this.eMailChangeHandle}
                             valueChangeEvent='input'
                         />
                     </Item>
@@ -52,7 +65,7 @@ export class login extends Component {
                         />
                     </Item>
                     <Item ratio={1}>
-                        <Button text='Login' />
+                        <Button text='Login' onClick={this.loginClickHandle} />
                     </Item>
                 </Box>
             </React.Fragment>
--- src/index.js
+++ src/index.js
@@ -4,5 +4,17 @@ import './index.css';
 import App from './App';
 import 'devextreme/dist/css/dx.common.css';
 import 'devextreme/dist/css/dx.dark.compact.css';
+import firebase from 'firebase';
+
+var config = {
+    apiKey: "AIzaSyBYU-IkqYqEhvL9QNFFUfPZF4DbjSTHnn0",
+    authDomain: "cryptex-72ee5.firebaseapp.com",
+    databaseURL: "https://cryptex-72ee5.firebaseio.com",
+    projectId: "cryptex-72ee5",
+    storageBucket: "cryptex-72ee5.appspot.com",
+    messagingSenderId: "1022914030119"
+  };
+
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
