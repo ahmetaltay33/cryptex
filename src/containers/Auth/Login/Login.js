9d96663f7777a37b5e2065fa4052b1165d3fd1a9
Ahmet Altay <ahmetaltay33@users.noreply.github.com>
2019-02-02 13:29:22 +0300

    reconfigured indet size; set 4 from 2

--- src/containers/Auth/Login/Login.js
+++ src/containers/Auth/Login/Login.js
@@ -8,75 +8,75 @@ import axios from 'axios';
 import firebase from 'firebase';
 
 export class login extends Component {
-    //static propTypes = {
-    //}
-    constructor(props) {
-        super(props);
-        this.state = {
-            eMail: '',
-            password: ''
-        };
-        this.eMailChangeHandle = this.eMailChangeHandle.bind(this);
-        this.passwordChangeHandle = this.passwordChangeHandle.bind(this);
-        this.loginClickHandle = this.loginClickHandle.bind(this);
-    }
+  //static propTypes = {
+  //}
+  constructor(props) {
+    super(props);
+    this.state = {
+      eMail: '',
+      password: ''
+    };
+    this.eMailChangeHandle = this.eMailChangeHandle.bind(this);
+    this.passwordChangeHandle = this.passwordChangeHandle.bind(this);
+    this.loginClickHandle = this.loginClickHandle.bind(this);
+  }
 
-    eMailChangeHandle(e) {
-        this.setState({
-            eMail: e.value
-        });
-    }
+  eMailChangeHandle(e) {
+    this.setState({
+      eMail: e.value
+    });
+  }
 
-    passwordChangeHandle(e) {
-        this.setState({
-            password: e.value
-        });
-    }
+  passwordChangeHandle(e) {
+    this.setState({
+      password: e.value
+    });
+  }
 
-    loginClickHandle() {
-        firebase.auth().signInWithEmailAndPassword(this.state.eMail, this.state.password)
-            .then(response => {
-                console.log(response);
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    }
+  loginClickHandle() {
+    firebase.auth().signInWithEmailAndPassword(this.state.eMail, this.state.password)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
 
-    render() {
-        return (
-            <React.Fragment>
-                <Box direction={'col'} width={'100%'} height={125} crossAlign="center" align="center">
-                    <Item ratio={1}>
-                        <header>Login Form</header>
-                    </Item>
-                    <Item ratio={1}>
-                        <TextBox
-                            //value={this.state.userName}
-                            onValueChanged={this.eMailChangeHandle}
-                            valueChangeEvent='input'
-                            mode='email'
-                            //validationError='E-mail address is not valid!'
-                            validationMessageMode='auto'
-                            maxLength='100'
-                        />
-                    </Item>
-                    <Item ratio={1}>
-                        <TextBox
-                            //value={this.state.password}
-                            onValueChanged={this.passwordChangeHandle}
-                            valueChangeEvent='input'
-                            mode='password'
-                            maxLength='10'
-                        />
-                    </Item>
-                    <Item ratio={1}>
-                        <Button text='Login' onClick={this.loginClickHandle} />
-                    </Item>
-                </Box>
-            </React.Fragment>
-        )
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <Box direction={'col'} width={'100%'} height={125} crossAlign="center" align="center">
+          <Item ratio={1}>
+            <header>Login Form</header>
+          </Item>
+          <Item ratio={1}>
+            <TextBox
+              //value={this.state.userName}
+              onValueChanged={this.eMailChangeHandle}
+              valueChangeEvent='input'
+              mode='email'
+              //validationError='E-mail address is not valid!'
+              validationMessageMode='auto'
+              maxLength='100'
+            />
+          </Item>
+          <Item ratio={1}>
+            <TextBox
+              //value={this.state.password}
+              onValueChanged={this.passwordChangeHandle}
+              valueChangeEvent='input'
+              mode='password'
+              maxLength='10'
+            />
+          </Item>
+          <Item ratio={1}>
+            <Button text='Login' onClick={this.loginClickHandle} />
+          </Item>
+        </Box>
+      </React.Fragment>
+    )
+  }
 }
 
 export default login;
---
+++ src/containers/vault/vault.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class vault extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default vault
